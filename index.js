f60658dee24f8c87db786eccdffc0b25aa8876af
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-21 21:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArr = []
+  for(i = 0; i < musicians.length - 1; i++){
+    emptyArr.push( `${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return emptyArr
+}
